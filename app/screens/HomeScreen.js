3860f398b4980830f2be25b4d9d8c1643ab45919
ChathuVithanage <chathuriimasha60@gmail.com>
2024-02-29 03:13:57 -0800

    first commit

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+
+import Screen from '../components/Screen';
+import AppButton from '../components/AppButton';
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <Screen>
+            <View style={styles.imageContainer}>
+                <Image style={styles.background} source={require("../assets/Home_03.png")}></Image>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" fontSize={18} height={50} onPress={()=>{navigation.navigate('Login')}}/>
+                <AppButton title="Register" fontSize={18} height={50} onPress={()=>{navigation.navigate('Sign Up')}}/>
+            </View>
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonsContainer: {
+        padding: 20,
+        width: "100%",
+        position:'absolute',
+        bottom:0
+    },
+    background: {
+        width: '100%',
+        height: "100%",
+        objectFit: 'contain',
+        opacity: 1,
+
+    }
+});
